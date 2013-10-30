2481cd8e5c982e9f9bc384437de39835c0926ee0
Fernando <fermayo@gmail.com>
2013-10-29 23:38:40 -0300

    added images and format

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-echo 'Hello world!\n';
-?>
+<html>
+<head>
+	<title>Hello world!</title>
+	<style>
+	body {
+		background-image: url('bg.png');
+		text-align: center;
+		padding: 50px;
+		font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+	}
+	</style>
+</head>
+<body>
+	<img id="logo" src="logo.png" />
+	<h1><?php echo "Hello world!"; ?></h1>
+</body>
+</html>
